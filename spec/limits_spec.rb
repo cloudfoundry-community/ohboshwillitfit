3966e7af61e55eaa0e5751d4a11c3d62f805bb2c
Dr Nic Williams <drnicwilliams@gmail.com>
2014-03-19 22:55:57 -0400

    An additional example fog limits json

--- spec/limits_spec.rb
+++ spec/limits_spec.rb
@@ -1,17 +1,19 @@
 require "json"
 
 describe OhBoshWillItFit::Limits do
-  let(:fog_limits) { JSON.parse(File.read(spec_asset("fog_limits.json"))) }
-  let(:get_limits) { instance_double("Excon::Response", data: { body: fog_limits} ) }
-  let(:fog_compute) { instance_double("Fog::Compute::OpenStack::Real", get_limits: get_limits) }
-  subject { OhBoshWillItFit::Limits.new(fog_compute) }
-  it {
-    expect(subject.max_total_cores).to eq(1000)
-  }
-  it {
-    expect(subject.max_total_instances).to eq(100)
-  }
-  it {
-    expect(subject.max_total_ram_size).to eq(1966080)
-  }
+  context "fog_limits_no_totals" do
+    let(:fog_limits) { JSON.parse(File.read(spec_asset("fog_limits_no_totals.json"))) }
+    let(:get_limits) { instance_double("Excon::Response", data: { body: fog_limits} ) }
+    let(:fog_compute) { instance_double("Fog::Compute::OpenStack::Real", get_limits: get_limits) }
+    subject { OhBoshWillItFit::Limits.new(fog_compute) }
+    it {
+      expect(subject.max_total_cores).to eq(1000)
+    }
+    it {
+      expect(subject.max_total_instances).to eq(100)
+    }
+    it {
+      expect(subject.max_total_ram_size).to eq(1966080)
+    }
+  end
 end
