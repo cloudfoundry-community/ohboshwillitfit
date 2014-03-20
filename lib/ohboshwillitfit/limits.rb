f7bfa1d2b7940af79bdf323abef7213662f386cc
Dr Nic Williams <drnicwilliams@gmail.com>
2014-03-19 21:54:16 -0400

    Implement Limits

--- lib/ohboshwillitfit/limits.rb
+++ lib/ohboshwillitfit/limits.rb
@@ -7,15 +7,19 @@ module OhBoshWillItFit
     end
 
     def max_total_cores
-
+      limits.data[:body]["limits"]["absolute"]["maxTotalCores"]
     end
 
     def max_total_instances
-
+      limits.data[:body]["limits"]["absolute"]["maxTotalInstances"]
     end
 
     def max_total_ram_size
+      limits.data[:body]["limits"]["absolute"]["maxTotalRAMSize"]
+    end
 
+    def limits
+      @limits ||= fog_compute.get_limits
     end
 
   end
